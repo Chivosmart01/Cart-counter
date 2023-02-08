fa875f7d170ce2beef1da098b3f9f406ad67c81c
Chivosmart <abdullahiyusufademola@gmail.com>
2023-02-08 12:38:47 +0100

    Complete cart-item counter project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [disabled, setDisabled] = useState(true);
+
+  const increaseCounter = () => {
+    setCounter((prev) => prev + 1);
+    if (counter > -1) {
+      setDisabled(false);
+    }
+  };
+
+  const decreaseCounter = () => {
+    if (counter === 0 ) {
+      setDisabled(true);
+      return;
+    }
+    setCounter((prev) => prev - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <h3>Number of items in Cart : {counter}</h3>
+      <div className="btns">
+        <button className="add-btn" onClick={increaseCounter}>
+          Add Item To Cart
+        </button>
+        <button
+          disable={disabled}
+          className={`"remove-btn" ${!disabled ? "valid" : ""}`}
+          onClick={decreaseCounter}
         >
-          Learn React
-        </a>
-      </header>
+          Remove Item From Cart
+        </button>
+      </div>
     </div>
   );
 }
